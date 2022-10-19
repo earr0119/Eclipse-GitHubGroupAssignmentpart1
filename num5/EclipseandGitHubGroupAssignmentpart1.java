5a1272e2e7e3aa1f93bc0530ac105c75a9717557
Elis Ruiz <earr0119@hotmail.com>
2022-10-18 20:14:15 -0400

    first commit in part1

---
+++ num5/EclipseandGitHubGroupAssignmentpart1.java
@@ -0,0 +1,12 @@
+package num5;
+
+public class EclipseandGitHubGroupAssignmentpart1 {
+
+	public static void main(String[] args) {
+		System.out.println("******************************************************");
+		System.out.println("**************Welcome to EarFactory*******************");
+		System.out.println("******************************************************\n");
+
+	}
+
+}
